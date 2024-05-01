a65c8310e142af96ea8f87b5ee47416892bf5bd1
Andrew Hale <services.hale.adh@gmail.com>
2024-05-01 17:04:32 -0400

    honor redirects

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -3,13 +3,14 @@
 namespace thisanimus\CCBLogin\controllers;
 
 use Craft;
+use yii\web\Response;
 use thisanimus\CCBLogin\services\CCBService;
 use craft\web\Controller;
 
 class DefaultController extends Controller {
 	protected int|bool|array $allowAnonymous = true;
 
-	public function actionIndex() {
+	public function actionIndex(): Response {
 		$ccb = new CCBService;
 		$this->requirePostRequest();
 		$request = Craft::$app->getRequest();
@@ -19,5 +20,7 @@ class DefaultController extends Controller {
 		} else {
 			$ccb->getUser($request->getBodyParam('formLogin'), $request->getBodyParam('formPassword'));
 		}
+		// Redirect the request based on a 'redirect' param
+		return $this->redirectToPostedUrl();
 	}
 }
